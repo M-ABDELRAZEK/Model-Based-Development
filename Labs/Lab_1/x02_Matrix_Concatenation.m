8d1c405245561e1c5e8641439417dc1aba41e121
M-ABDELRAZEK <mohamedabdelrazek1513@gmail.com>
2024-09-17 01:28:01 +0300

    Lab1

---
+++ Labs/Lab_1/x02_Matrix_Concatenation.m
@@ -0,0 +1,5 @@
+rowVector = [1:5];
+columnVector = [6:10];
+
+horizontalConcat = horzcat(rowVector, columnVector);
+disp(horizontalConcat)
---
+++ Labs/Lab_1/x03_Using_repmat_Function.m
@@ -0,0 +1,4 @@
+originalMatrix = [1 2; 3 4];
+
+repeatedMatrix = repmat(originalMatrix, 2, 2);
+disp(repeatedMatrix)
