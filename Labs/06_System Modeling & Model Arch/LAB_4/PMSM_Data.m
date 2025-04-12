93c455ef5056b0e7a59b228c026116b107e3563c
M-ABDELRAZEK <mohamedabdelrazek1513@gmail.com>
2025-04-12 15:40:32 +0200

    MBD

---
+++ Labs/06_System Modeling & Model Arch/LAB_4/PMSM_Data.m
@@ -0,0 +1,10 @@
+R = 1; % Stator Resistance
+Ls = 0.01; % Stator Inductunce
+Lr = 0.005; % Rotor Inductance
+Lm = 0.02; % Magnitization Inductance
+Sigma = 0.1; % Leakage Factor
+P = 2; % Number Of Pair Poles
+
+Vpeak = 12;
+Ipeak = 2;
+w = 300;
